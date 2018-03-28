7e88969ba49d08f2b5b5e41b97417d50051e8aea
saurabh.sharma <saurabh.sharma@spicedigital.in>
2018-03-28 16:17:03 +0530

    dd

---
+++ public/client/constant.js
@@ -0,0 +1,18 @@
+"use strict";
+
+app.constant("configuration", (function() {
+    return {
+
+
+         //BASE_URL: window.location.protocol + '//' + window.location.hostname + ':3000/',       //This is base url for HTTP POST, GET, PUT, DELETE Methods
+        BASE_URL: window.location.protocol + '//' + window.location.hostname + '/',       //This is base url for HTTP POST, GET, PUT, DELETE Methods
+         //SECOND_URL: window.location.protocol + '//' + window.location.hostname + ':3000/' ,      //This is second url for HTTP POST, GET, PUT, DELETE Methods
+
+        SECOND_URL: window.location.protocol + '//' + window.location.hostname + '/' ,      //This is second url for HTTP POST, GET, PUT, DELETE Methods
+
+        
+        SIGN_UP_URL: "api/signup",
+        LOGIN_URL : "api/login"
+
+    }
+})());
--- public/client/userController.js
+++ public/client/userController.js
@@ -1,5 +1,16 @@
 "use strict";
 
-app.controller('userController', function($scope, $http, $location){
-	
+app.controller('userController', function($scope, $http,configuration, $location){
+
+
+	$scope.userLogin = function(){ 
+	 httpd.post(configuration.EDIT_ITEMS, $scope.newInvoice).then(function success(res){
+                    $scope.SuccessMsg = res.data.message;  
+                }, function errorCallback(err){
+                    $scope.invoiceError = true;
+                    $scope.invoiceErrorMsg = err.data.message; 
+
+                });
+	}
+
 });
